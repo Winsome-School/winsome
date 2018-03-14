f695836e62d151d11ed3779f4151e48a9e2136d3
Ahmad <ahmad.ramay4@gmail.com>
2018-03-14 14:03:21 +0500

    ahmadAfzal

---
+++ footer/src/js/secondary/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Images from './images';
+import Footer from '../footer';
+class App extends React.Component{
+
+	render(){
+		return(
+			<div>
+				<Footer />
+			</div>
+		)
+	};
+}
+export default App;
