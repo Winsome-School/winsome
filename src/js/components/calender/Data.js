034f080082c9a11791201b19142a87149d3f48ce
Rehan712 <rehanbhatti712@gmail.com>
2018-03-15 14:24:29 +0500

    Final Change from Rehan Saeed

--- src/js/components/calender/Data.js
+++ src/js/components/calender/Data.js
@@ -2,62 +2,62 @@ var events=[
 	{
 		id:1,
 		event:'Half terms End',
-		eventDate:'1st May',
+		eventDate:'01 May',
 		time:''
 
 	},
 	{
 		id:2,
 		event:'NetBall: first vii vs viii',
-		eventDate:'20th May',
+		eventDate:'20 May',
 		time:'2:30 pm'
 
 	},
 	{
 		id:3,
 		event:'Parent presentation on healthy living',
-		eventDate:'3rd June',
+		eventDate:'03 June',
 		time:'12:35 pm'
 
 	},
 	{
 		id:4,
 		event:'M5 rs trip to walsigham',
-		eventDate:'25th June',
+		eventDate:'25 June',
 		time:''
 	},
 	{
 		id:5,
 		event:'Tour to muree',
-		eventDate:'15th July',
+		eventDate:'15 July',
 		time:'2:30 pm'
 
 	},
 	{
 		id:6,
 		event:'Water splash Day',
-		eventDate:'28th July',
+		eventDate:'28 July',
 		time:'11:35 am'
 
 	},
 	{
 		id:7,
 		event:'Visit to Jungle',
-		eventDate:'5th Aug',
+		eventDate:'05 Aug',
 		time:''
 
 	},
 	{
 		id:8,
 		event:'Independence Day',
-		eventDate:'14th Aug',
+		eventDate:'14 Aug',
 		time:'12:35 am'
 
 	},
 	{
 		id:9,
 		event:'Bsant Day',
-		eventDate:'25th Aug',
+		eventDate:'25 Aug',
 		time:'11:38 pm'
 
 	}
