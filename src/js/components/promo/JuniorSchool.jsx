a4a55344a0a09e84527e0d03c52de75de8213dba
Taimoor Khan <taimoor.m.k@live.com>
2018-03-16 14:15:10 +0500

    junior school heading updated

--- src/js/components/promo/JuniorSchool.jsx
+++ src/js/components/promo/JuniorSchool.jsx
@@ -13,7 +13,7 @@ export default class JuniorSchool extends Component {
       <section id="junior-school">
         <div className="top-section">
           <div className="wrapper">
-            <h4 className="junior-school-heading">Junior School</h4>
+            <h3 className="junior-school-heading">Junior School</h3>
             <p className="junior-school-description">At Winsome we believe in providing a dual approach to education which supports every child to become confident learners. For us, the quality of relationship developed with the child has a direct link to the effective practice of teaching the children and guiding them to discover new things and think for themselves. We believe that if the teacher-child relationship is based on mutual respect, active listening and the sharing of knowledge and skills, the child’s capacity to learn and discover the world is enhanced.</p>
               <ReadMoreButton 
                 align="center"
