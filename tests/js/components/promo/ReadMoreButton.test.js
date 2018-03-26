1d67b32a862092fdeeffb7e3f3654fcc70bdc729
Taimoor Khan <taimoor.m.k@live.com>
2018-03-26 21:48:44 +0500

    ReadMoreButton testing deployed

---
+++ tests/js/components/promo/ReadMoreButton.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import ReadMoreButton from '../../../../src/js/components/promo/ReadMoreButton';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('testing ReadMoreButton component', () => {
+  const wrapper = shallow(<ReadMoreButton />);
+  it('renders the ReadMoreButton component', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
