179a9f8df7602d9d6f560bb3d211ab76d482783e
Taimoor Khan <taimoor.m.k@live.com>
2018-03-26 21:25:58 +0500

    Promo partial testing deployed

--- tests/js/components/promo/Promo.test.js
+++ tests/js/components/promo/Promo.test.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Promo from '../../../../src/js/components/promo/app';
+import { Promo } from '../../../../src/js/components/promo/Promo';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('testing Promo component', () => {
-  const wrapper = shallow(<Promo />);
+  const promoData = [
+    {id: 1},
+    {id: 2}
+  ]
+  const wrapper = shallow(<Promo promoData={promoData} id={promoData[0].id}/>);
   it('renders the Promo component', () => {
     expect(wrapper).toMatchSnapshot();
   });
