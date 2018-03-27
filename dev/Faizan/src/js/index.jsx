6eac965be01de0ef22d1f3932071d62483279326
faizanzaheer <faizan@DESKTOP-47DHGLU.localdomain>
2018-03-27 09:11:36 -0700

    FaizanTask

---
+++ dev/Faizan/src/js/index.jsx
@@ -0,0 +1,9 @@
+import '../css/styles.scss'
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Contact from './Contact';
+
+ReactDOM.render(
+  <Contact />,
+  document.querySelector('#app')
+);
