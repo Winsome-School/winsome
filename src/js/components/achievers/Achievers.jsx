482cb60524c1c8592edeae2f6de98e94d457ce36
Taimoor Khan <taimoor.m.k@live.com>
2018-03-28 15:24:55 +0500

    Added Discover into Achievers page

--- src/js/components/achievers/Achievers.jsx
+++ src/js/components/achievers/Achievers.jsx
@@ -4,53 +4,56 @@ import PropTypes from 'prop-types'
 
 import FirstHeading from './FirstHeading'
 import SecondPart2 from './SecondPart2';
-import {Achieversdata} from './data';
+import { Achieversdata } from './data';
 import News from '../News/News';
 import Discover from '../discover/Discover';
 
 
- class Achievers extends React.Component {
-	constructor(){
-		super();
-	}
-	renderArray(){
+class Achievers extends React.Component {
+  constructor() {
+    super();
+  }
+  renderArray() {
 
-	}
-	render(){
-		
-		return (
-			<div className="achievers">
-			<div className="box1"></div>
-				<FirstHeading 
-					heading={Achieversdata.firstPart.heading}
-					text={Achieversdata.firstPart.text}
-				/>
-				<div className="clear"></div>
+  }
+  render() {
 
-				{
-				// 	<SecondPart2
-				//   top={0}
-				//  firstNo={3} 
-				//  secondNo={5} 
-				//  heading={Achieversdata.classYear}
-				//  classStudents={Achieversdata.classOf2017}
-				 
-				// />
-			}
-				 <SecondPart2  
-				 top={0}
-				 firstNo={2} 
-				 secondNo={3} 
-				 heading={Achieversdata.classYear2}
-				 classStudents={[Achieversdata.classOf2016]}
-				/>
+    return (
+      <div>
+        <div className="achievers">
+          <div className="box1"></div>
+          <FirstHeading
+            heading={Achieversdata.firstPart.heading}
+            text={Achieversdata.firstPart.text}
+          />
+          <div className="clear"></div>
 
-        <News />
+          {
+            // 	<SecondPart2
+            //   top={0}
+            //  firstNo={3} 
+            //  secondNo={5} 
+            //  heading={Achieversdata.classYear}
+            //  classStudents={Achieversdata.classOf2017}
+
+            // />
+          }
+          <SecondPart2
+            top={0}
+            firstNo={2}
+            secondNo={3}
+            heading={Achieversdata.classYear2}
+            classStudents={[Achieversdata.classOf2016]}
+          />
+
+          <News />
+
+          <div className="clear"></div>
+        </div>
         <Discover />
-				<div className="clear"></div>
-			</div>
-		)
-	}
-} 
+      </div>
+    )
+  }
+}
 
 export default Achievers
