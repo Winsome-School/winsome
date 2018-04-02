5a279ef2d5ddf7a9b48cc5246e668047d6094cc8
Rehan712 <rehanbhatti712@gmail.com>
2018-04-02 16:45:39 +0500

    minor Changes

--- src/js/components/calender/Arrow.jsx
+++ src/js/components/calender/Arrow.jsx
@@ -5,24 +5,14 @@ import * as actions from '../../actions';
 
 export const Arrow = ({ onClickRightArrow, onClickLeftArrow }) => (
 	<div>
-		<div
-			className="arrowRight"
-			onClick={onClickRightArrow}
-			role="button"
-			tabIndex="0"
-		/>
-		<div
-			className="arrowLeft"
-			onClick={onClickLeftArrow}
-			role="button"
-			tabIndex="0"
-		/>
+		<div className="arrowRight" onClick={onClickRightArrow} />
+		<div className="arrowLeft" onClick={onClickLeftArrow} />
 	</div>
 );
 
 Arrow.propTypes = {
-	onClickRightArrow: func,
-	onClickLeftArrow: func
+	onClickRightArrow: func.isRequired,
+	onClickLeftArrow: func.isRequired
 };
 export default connect(null, {
 	onClickLeftArrow: actions.onClickLeftArrow,
