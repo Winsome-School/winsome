8ab4a729917124f3fd9b65e99116732ccacc3cd9
Taimoor Khan <taimoor.m.k@live.com>
2018-04-04 15:51:44 +0500

    PromoCard optimised

--- src/js/components/promo/PromoCard.jsx
+++ src/js/components/promo/PromoCard.jsx
@@ -7,35 +7,28 @@ import { shape, number, string } from 'prop-types';
 // ########## Import Components Here ##########
 import ReadMoreButton from './ReadMoreButton';
 
-export default class PromoCard extends Component {
-
-  componentDidMount() {
-
-  }
-
-  render() {
-    const { data } = this.props;
-    return (
-      <div className="promo-card">
-        <div className="wrapper">
-          <div className={`promo-card-container-${data.promoCardId > 2 && "sectionTwo-" || data.promoCardId <= 2 && "sectionOne-"}${data.imageDirection}`}>
-            <div className={`promo-image-container-${data.promoCardId > 2 && "sectionTwo-" || data.promoCardId <= 2 && "sectionOne-"}${data.imageDirection}`}>
-              <div className={`promo-image-${data.promoCardId > 2 && "sectionTwo-" || data.promoCardId <= 2 && "sectionOne-"}${data.imageDirection}`} />
-            </div>
-            <div className="promo-text-container">
-              <div className="promo-text-wrapper">
-                <h4 className="promo-heading">{data.title}</h4>
-                <p className="promo-description">{data.description}</p>
-                <ReadMoreButton 
-                  align="left"
-                />
-              </div>
+const PromoCard = (props) => {
+  const { data } = props;
+  return (
+    <div className="promo-card">
+      <div className="wrapper">
+        <div className={`promo-card-container-${data.promoCardId > 2 && "sectionTwo-" || data.promoCardId <= 2 && "sectionOne-"}${data.imageDirection}`}>
+          <div className={`promo-image-container-${data.promoCardId > 2 && "sectionTwo-" || data.promoCardId <= 2 && "sectionOne-"}${data.imageDirection}`}>
+            <div className={`promo-image-${data.promoCardId > 2 && "sectionTwo-" || data.promoCardId <= 2 && "sectionOne-"}${data.imageDirection}`} />
+          </div>
+          <div className="promo-text-container">
+            <div className="promo-text-wrapper">
+              <h4 className="promo-heading">{data.title}</h4>
+              <p className="promo-description">{data.description}</p>
+              <ReadMoreButton
+                align="left"
+              />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 PromoCard.propTypes = {
@@ -46,3 +39,5 @@ PromoCard.propTypes = {
     description: string.isRequired
   }).isRequired
 }
+
+export default PromoCard;
