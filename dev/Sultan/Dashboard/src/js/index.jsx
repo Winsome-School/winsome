72a2737b0640b25bc61915aeaf92b6ca8e305148
Sultan Aslam <sultanaslam93@gmail.com>
2018-04-04 23:55:14 +0500

    DASHBOARD ICON BOXES

---
+++ dev/Sultan/Dashboard/src/js/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Dashboard from './Dashboard';
+
+ReactDOM.render(
+	<Dashboard />,
+	document.querySelector('#app')
+);
--- src/js/components/photogallery/PhotoGallery.jsx
+++ src/js/components/photogallery/PhotoGallery.jsx
@@ -1,45 +1,40 @@
 import React from 'react';
 import Discover from '../discover/Discover';
-import { images } from './photogallerydata';
+import images from './photogalleryData';
 import Hero from '../hero/HeroImage';
 
-const Gallery = () => {
-  return (
-    <div>
-      <Hero
-        style={'header-image'}
-      />
-      <div id='photogallery'>
-        <div>
-          <h2 className='photogalleryH2'>Photo Gallery Title</h2>
-          <div className='yellowBox'></div>
-          <p className='photogalleryP'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
-        	</p>
-        </div>
-        <div className='showImages'>
-          {showImages()}
-        </div>
+function showImages(){
+  return images.map((item)=>(
+    <div key={item.key} className='imageContainer'>
+      <div className='img' style={{backgroundImage: `url('${item.path}')`}} />
+      <div className='photoCaption'>
+        <p>Photo Caption. {item.caption}</p>
       </div>
-      <Discover />
     </div>
-  )
-}
+  ));
+};
 
-function showImages() {
-  return images.map((item, i) => {
-    return (
-      <div key={item.key} className='imageContainer'>
-        <div className='img' style={{ backgroundImage: 'url(' + item.path + ')' }}>
-        </div>
-        <div className='photoCaption'>
-          <p>Photo Caption. {item.caption}</p>
-        </div>
-      </div>)
-  })
-}
-
+const Gallery = () => (
+  <div>
+    <Hero
+      style={'header-image'}
+    />
+    <div id='photogallery'>
+      <div>
+        <h2 className='photogalleryH2'>Photo Gallery Title</h2>
+        <div className='yellowBox' />
+        <p className='photogalleryP'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore 
+          magna aliqua.
+        </p>
+      </div>
+      <div className='showImages'>
+        {showImages()}
+      </div>
+    </div>
+    <Discover />
+  </div>
+);
 
 export default Gallery
