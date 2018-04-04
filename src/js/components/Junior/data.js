39cedfe5a48023b57ee7ae1df0297ee3a0f7802a
shzad78 <shzad78@gmail.com>
2018-04-04 01:41:50 -0700

    'junoir-data'

---
+++ src/js/components/Junior/data.js
@@ -0,0 +1,29 @@
+export const data = 
+    {
+        p1:{
+          heading:"Junior School",
+          text:"Kindergarten and Pre-primary education provides an important foundation for both brain development and instilling a child's life long love of learning. We have a holistic view of education and cater for the physical, creative, intellectual, emotional, social and spiritual domains of learning in our classrooms.",
+          imageLink: "../image/junior1.jpg"
+            },          
+        p2:{
+            heading:"Creating Confident Learners",
+            imageLink: "../image/junior2.jpg",
+            p21:{
+                text: "At Winsome we believe in providing a dual approach to education which supports every child to become condent learners. For us, the quality of relationship developed with the child has a direct link to the eective practice of teaching the children and guiding them to discover new things and think for themselves. We believe that if the teacher-child relationship is based on mutual respect, active listening and the sharing of knowledge and skills, the child’s capacity to learn and discover the world is enhanced.",
+                },
+            p22:{
+                text: "We aim to make strong genuine connections with families to support the children's learning. We foster a shared sense of achievement through regular communication and by providing opportunities for parent and caregiver involvement in the classroom. Throughout the year there will be a number of special events and opportunities in which parents and caregivers can become involved.",
+                },
+            p23:{
+                text:"Our goal is to capture the heart of a child through play-based learning, which develops a love and excitement for learning—the foundation on which social, emotional, psychological, spiritual and creative wellbeing is developed and ultimately sustained into adulthood."
+                }   
+            },
+        p3:{
+            heading:"Key Features",
+            text:"Early Years programme promotes age-appropriate, holistic development focusing on children’s communication and language skills, creative expressions, as well as physical, social and personal competencies.",
+            
+            }
+      
+      
+    }
+ 
