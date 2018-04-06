1140c3e2de835b644e5795db3bdc8763b528169b
shahzad babar <shzad78@gmail.com>
2018-04-06 22:11:38 +0500

    junior

--- src/js/components/Junior/Junior.jsx
+++ src/js/components/Junior/Junior.jsx
@@ -1,67 +1,51 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Discover from '../discover/Discover';
-import Hero from '../hero/HeroImage';
-import News from '../News/News';
+import React from "react";
+import { connect } from "react-redux";
+import Discover from "../discover/Discover";
+import Hero from "../hero/HeroImage";
+import News from "../News/News";
 
- const Junior =  ({juniorData}) =>  (
-  
-      <div>
-        <Hero
-          style={'header-image'}
-        />
-        <div className="container">
-        
-          <div className="junior">
-            <h2>{juniorData.p1.heading}</h2>
-            <div className='box' />
-            <p>
-            {juniorData.p1.text}
-            </p>
-            <div className="image" >
-              <img src='./images/junior1.jpg' width='90%' alt="Junior1" />
-            </div>
-            
-          </div>
-          <News />
-          <div className="creating">
-            <h2>{juniorData.p2.heading}</h2>
-            <div className="box" />
-            <div className="textimage">
-              <div className="pdivs">
-                <p>
-                {juniorData.p2.p21.text}
-                </p>
-                <p>
-                {juniorData.p2.p22.text}
-                </p>
-                <p>
-                {juniorData.p2.p23.text}
-                </p>
-              </div>
-              <div className="image">
-                <img className='imglion' src='./images/junior2.jpg' alt='junior2' />
-              </div>
-              <div className="clear" />
-            </div>
-          </div>
-          <div className="key">
-            <h2>{juniorData.p3.heading}</h2>
-            <div className="box" />
-            <p>
-            {juniorData.p3.text}
-            </p>
-          </div>
+export const Junior = ({ juniorData }) => (
+  <div>
+    <Hero style={"header-image"} />
+    <div className="container">
+      <div className="junior">
+        <h2>{juniorData.p1.heading}</h2>
+        <div className="box" />
+        <p>{juniorData.p1.text}</p>
+        <div className="image">
+          <img src="./images/junior1.jpg" width="90%" alt="Junior1" />
         </div>
-        <Discover />
       </div>
-     
-  )
+      <News />
+      <div className="creating">
+        <h2>{juniorData.p2.heading}</h2>
+        <div className="box" />
+        <div className="textimage">
+          <div className="pdivs">
+            <p>{juniorData.p2.p21.text}</p>
+            <p>{juniorData.p2.p22.text}</p>
+            <p>{juniorData.p2.p23.text}</p>
+          </div>
+          <div className="image">
+            <img className="imglion" src="./images/junior2.jpg" alt="junior2" />
+          </div>
+          <div className="clear" />
+        </div>
+      </div>
+      <div className="key">
+        <h2>{juniorData.p3.heading}</h2>
+        <div className="box" />
+        <p>{juniorData.p3.text}</p>
+      </div>
+    </div>
+    <Discover />
+  </div>
+);
 
-  function mapStateToProps({juniorData}){
-    return {
-      juniorData
-    };
-  }
-  
+function mapStateToProps({ juniorData }) {
+  return {
+    juniorData
+  };
+}
+
 export default connect(mapStateToProps, null)(Junior);
