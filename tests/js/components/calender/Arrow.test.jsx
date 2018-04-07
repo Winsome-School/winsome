4cde310fd0a0366a2d73f6bee38fdd4055bff8e2
Rehan712 <rehanbhatti712@gmail.com>
2018-04-07 14:21:39 +0500

    minor changes in calender tests

--- tests/js/components/calender/Arrow.test.jsx
+++ tests/js/components/calender/Arrow.test.jsx
@@ -2,33 +2,48 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Arrow } from '../../../../src/js/components/calender/Arrow';
+import * as constants from '../../../../src/js/constants';
+import * as actions from '../../../../src/js/actions';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Test the Arrow Component', () => {
+	const onClickLeftArrow = jest.fn();
+	const onClickRightArrow = jest.fn();
+
 	const wrapper = shallow(<Arrow />);
 
 	it('checks the rendring of the Arrow component', () => {
 		expect(wrapper).toMatchSnapshot();
 	});
 	it('checks the Left Click', () => {
-		const onClickLeftArrow = jest.fn();
 		wrapper.find('.arrowLeft').simulate('click');
 		expect(onClickLeftArrow.mock.calls.length).toBe(0);
 	});
 	it('checks the Right Click', () => {
-		const onClickRightArrow = jest.fn();
 		wrapper.find('.arrowRight').simulate('click');
 		expect(onClickRightArrow.mock.calls.length).toBe(0);
 	});
 	it('checks the Left Enter key Press', () => {
-		const onClickLeftArrow = jest.fn();
 		wrapper.find('.arrowLeft').simulate('onkeypress');
 		expect(onClickLeftArrow.mock.calls.length).toBe(0);
 	});
 	it('checks the Right Click', () => {
-		const onClickRightArrow = jest.fn();
 		wrapper.find('.arrowRight').simulate('onkeypress');
 		expect(onClickRightArrow.mock.calls.length).toBe(0);
+	});
+	it('should test the onClickRightArrow function', () => {
+		const expectedActionRightArrow = {
+			type: constants.ARROW_RIGHT,
+			payload: 1
+		};
+		expect(actions.onClickRightArrow()).toEqual(expectedActionRightArrow);
+	});
+	it('should test the onClickLeftArrow function', () => {
+		const expectedActionLeftArrow = {
+			type: constants.ARROW_LEFT,
+			payload: 1
+		};
+		expect(actions.onClickLeftArrow()).toEqual(expectedActionLeftArrow);
 	});
 });
