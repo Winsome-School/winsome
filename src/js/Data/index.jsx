4a3de185254430c74258d6101fb378fe2eb9ca28
root <root@Ahxan.localdomain>
2018-04-15 12:37:00 +0500

    optimised

--- src/js/Data/index.jsx
+++ src/js/Data/index.jsx
@@ -2,38 +2,44 @@ const data = [
 	{
 		id: Math.random(),
 		img: 'blog-img1.jpg',
-		title: 'BLOG TITLE1',
-		description: 'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.' 
+		title: 'ismail',
+		description:
+			'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.'
 	},
 	{
 		id: Math.random(),
 		img: 'blog-img2.jpg',
 		title: 'BLOG TITLE2',
-		description: 'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.' 
+		description:
+			'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.'
 	},
 	{
 		id: Math.random(),
 		img: 'IMG_0224.jpg',
 		title: 'BLOG TITLE3',
-		description: 'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.' 
+		description:
+			'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.'
 	},
 	{
 		id: Math.random(),
 		img: 'IMG_0348.jpg',
 		title: 'BLOG TITLE4',
-		description: 'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.' 
+		description:
+			'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.'
 	},
 	{
 		id: Math.random(),
 		img: 'IMG_9439.jpg',
 		title: 'BLOG TITLE5',
-		description: 'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.' 
+		description:
+			'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.'
 	},
 	{
 		id: Math.random(),
 		img: 'IMG_9463.jpg',
 		title: 'BLOG TITLE6',
-		description: 'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.' 
+		description:
+			'Congratulations to our Year 6 boys (along with 3 of our Year 5’s) who won the WACSSA Division 1 Football Tournament today.'
 	}
 ];
 export default data;
